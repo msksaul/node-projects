868ca1ca839a646157c4a9cda1d7036c4775e872
mscalel <msksaul@gmail.com>
2022-10-04 17:14:49 -0600

    Introduction

---
+++ 1-node-tutorial/3-modules.js
@@ -0,0 +1,14 @@
+// CommonJs, every file is module (by default)
+// Modules - Encapsulated Code (only share minimun)
+
+const names = require('./4-names')
+const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavor')
+
+console.log(data)
+
+require('./7-mind-grenade')
+
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
