458eb5164c3aa3984cb75c5fe0eaa7027eb43725
mscalel <msksaul@gmail.com>
2022-10-13 15:14:32 -0600

    Jobs API, CRUD

--- 6-jobs-api/controllers/jobs.js
+++ 6-jobs-api/controllers/jobs.js
@@ -1,23 +1,63 @@
-
+const Job = require('../models/Job')
+const { StatusCodes } = require('http-status-codes')
+const { BadRequestError, NotFoundError } = require('../errors')
 
 const getAllJobs = async (req, res) => {
-  res.send('get all jobs')
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt')
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length })
 }
 
 const getJob = async (req, res) => {
-  res.send('get single job')
+  const { user: { userId }, params: { id: jobId }} = req
+
+  const job = await Job.findOne({
+    _id: jobId,
+    createdBy: userId
+  })
+  if(!job) {
+    throw new NotFoundError(`No job with id ${jobId}`)
+  }
+  res.status(StatusCodes.OK).json({ job })
 }
 
 const createJob = async (req, res) => {
-  res.send('create job')
+  req.body.createdBy = req.user.userId
+  const job = await Job.create(req.body)
+  res.status(StatusCodes.CREATED).json({ job })
 }
 
 const updateJob = async (req, res) => {
-  res.send('update job')
+  const { 
+    user: { userId }, 
+    params: { id: jobId },
+    body: { company, position }
+  } = req
+
+  if(!company === '' || position === '') {
+    throw new BadRequestError('Company or Position fields cannot be empty')
+  }
+  const job = await Job.findOneAndUpdate({ _id: jobId, createdBy: userId }, req.body, {  new: true, runValidators: true })
+  if(!job) {
+    throw new NotFoundError(`No job with id ${jobId}`)
+  }
+  res.status(StatusCodes.OK).json({ job })
 }
 
 const deleteJob = async (req, res) => {
-  res.send('delete job')
+  const { 
+    user: { userId }, 
+    params: { id: jobId },
+    body: { company, position }
+  } = req
+
+  const job = await Job.findOneAndDelete({
+    _id: jobId,
+    createdBy: userId
+  })
+  if(!job) {
+    throw new NotFoundError(`No job with id ${jobId}`)
+  }
+  res.status(StatusCodes.OK).send()
 }
 
 
--- 6-jobs-api/models/User.js
+++ 6-jobs-api/models/User.js
@@ -34,4 +34,9 @@ UserSchema.methods.createJWT = function() {
   return jwt.sign({ userId: this._id, name: this.name }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_LIFETIME })
 }
 
+UserSchema.methods.comparePassword = async function(candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password)
+  return isMatch
+}
+
 module.exports = mongoose.model('User', UserSchema)
